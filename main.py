51d40ab8e30f9563f4ba65fcc7b6f46016b48287
ZeroFourZura <ernis2580@gmail.com>
2020-08-25 18:53:56 +0300

    Added extra command. I'm learning this stuff bit by bit

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ from discord.ext import commands
 
 
 #Using this prefix for testing purposes, can be changed later to anything we decide
-bot = commands.Bot(coommand_prefix='?')
+bot = commands.Bot(command_prefix='?')
 
 @bot.event()
-async def on_ready():
+async def on_ready(ctx):
     print('Bot is ready.')
+    #This bellow command? Yes, it doesn't work :(
+    await ctx.send("Hey you're finally awake... NO! I am finally awake")
 
+@bot.command()
+async def Test(ctx):
+    await ctx.send('Yo!')
 
 #I think that other people shouldn't see the bot.run token, so I'll leave it blank
 #in the github and add it when we need it
