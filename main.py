b386d56af4b5468fae23ed8006f1c2de1aad05c7
lvl67paladin <9xchains@gmail.com>
2020-09-01 16:23:01 +0530

    updates

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ from discord.ext import commands
 #Using this prefix for testing purposes, can be changed later to anything we decide
 bot = commands.Bot(command_prefix='?')
 
-@bot.event()
-async def on_ready(ctx):
-    print('Bot is ready.')
-    #This bellow command? Yes, it doesn't work :(
-    await ctx.send("Hey you're finally awake... NO! I am finally awake")
+
 
 @bot.command()
 async def Test(ctx):
@@ -17,4 +13,7 @@ async def Test(ctx):
 
 #I think that other people shouldn't see the bot.run token, so I'll leave it blank
 #in the github and add it when we need it
-bot.run('')
+try:
+    bot.run('token')
+except Exception as e:
+    print(f"problem logging in:{e}")
