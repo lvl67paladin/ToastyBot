4dc8c57da43f47fd38181986d152ef9086138cb0
ZeroFourZura <ernis2580@gmail.com>
2020-09-04 20:09:23 +0300

    Implementing cogs and cleaning up

---
+++ Cogs/Events.py
@@ -0,0 +1,61 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+import pymongo
+from pymongo import MongoClient
+
+load_dotenv()
+
+db_cluster = os.getenv('db_cluster')
+
+cluster = MongoClient(db_cluster)
+db = cluster["bot_user_data"]
+collection = db["bot_user"]
+# Same reason as I mentioned in Commans.py
+
+class Events(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Event Cog is ready for some action")
+
+    #Dd's messy command. It also doesn't work
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        #print(f"{ctx.channel}: {ctx.author}: {ctx.author.name}: {ctx.content}")
+        myquery = {"_id": ctx.author.id}
+        #first check if the message is from bot itelf
+        if ctx.author==self.bot.user:
+            await self.bot.process_commands(ctx)
+
+        if "stream" in str(ctx.content.lower()):
+            await ctx.channel.send("simp!")
+
+        if "money" in str(ctx.content.lower()):
+            await ctx.channel.send("here! take some coins you pityful creature")
+
+        if (collection.count_documents(myquery) == 0):
+            if "crispy" in str(ctx.content.lower()):
+                post = {"_id": ctx.author.id, "score": 1}
+                collection.insert_one(post)
+                await ctx.channel.send('accepted!')
+        else:
+            if "crispy" in str(ctx.content.lower()):
+                query = {"_id": ctx.author.id}
+                user = collection.find(query)
+                for result in user:
+                    score = result["score"]
+                score = score + 1
+                collection.update_one({"_id":ctx.author.id}, {"$set":{"score":score}})
+                await ctx.channel.send('accepted!')
+        #very important..we need to call this otherwise commands won't work
+        await self.bot.process_commands(ctx)
+
+
+def setup(bot):
+    bot.add_cog(Events(bot))
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import os
 from dotenv import load_dotenv
 import discord
@@ -18,58 +19,33 @@ cluster = MongoClient(db_cluster)
 db = cluster["bot_user_data"]
 collection = db["bot_user"]
 
-@bot.event
-async def on_ready(ctx):
-    print("logging in")
-    await  ctx.send("logged in")
 
+# Trying to implement cogs idk.
+#A command to load a cog
 @bot.command()
-async def test(ctx):
-    await ctx.send('Yo!')
+async def load(ctx, extension):
+    bot.load_extension(f'Cogs.{extension}')
 
-
-
-
-
-@bot.event
-async def on_message(ctx):
-    #print(f"{ctx.channel}: {ctx.author}: {ctx.author.name}: {ctx.content}")
-    myquery = {"_id": ctx.author.id}
-    #first check if the message is from bot itelf
-    if ctx.author==bot.user:
-        await bot.process_commands(ctx)
-
-    if "stream" in str(ctx.content.lower()):
-        await ctx.channel.send("simp!")
-
-    if "money" in str(ctx.content.lower()):
-        await ctx.channel.send("here! take some coins you pityful creature")
-    
-    if (collection.count_documents(myquery) == 0):
-        if "crispy" in str(ctx.content.lower()):
-            post = {"_id": ctx.author.id, "score": 1}
-            collection.insert_one(post)
-            await ctx.channel.send('accepted!')
-    else:
-        if "crispy" in str(ctx.content.lower()):
-            query = {"_id": ctx.author.id}
-            user = collection.find(query)
-            for result in user:
-                score = result["score"]
-            score = score + 1
-            collection.update_one({"_id":ctx.author.id}, {"$set":{"score":score}})
-            await ctx.channel.send('accepted!')
-    #very important..we need to call this otherwise commands won't work
-    await bot.process_commands(ctx)
-   
+#A command to unload a cog
+#Tho, idk if we need load and unload process_commands
+#So, random peeps won't be able to unload the commands
+#It might be usefull for testing stuff tho
 @bot.command()
-async def score(ctx):
-    score=collection.find_one({"_id": ctx.author.id})
-    await ctx.send(score["score"])
+async def unload(ctx, extension):
+    bot.unload_extension(f'Cogs.{extension}')
+
+#Another usefull testing command for relading cogs
+@bot.command()
+async def reload(ctx, extension):
+    bot.unload_extension(f'Cogs.{extension}')
+    bot.load_extension(f'Cogs.{extension}')
+
+#Loading all cogs
+for filename in os.listdir('./Cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'Cogs.{filename[:-3]}')
 
 
-#I think that other people shouldn't see the bot.run token, so I'll leave it blank
-#in the github and add it when we need it
 try:
     bot.run(token)
 except Exception as e:
